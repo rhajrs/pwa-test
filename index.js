33a2afbb777a079ea971c26f9c1f7d427517ff3f
rhajrs <34175227+rhajrs@users.noreply.github.com>
2020-04-13 01:45:33 +0900

    remove alert messages

--- index.js
+++ index.js
@@ -24,10 +24,8 @@ if('serviceWorker' in navigator) {
 let deferredPrompt;
 const addBtn = document.querySelector('.add-button');
 addBtn.style.display = 'none';
-alert('test22');
 
 window.addEventListener('beforeinstallprompt', (e) => {
-	alert('test');
   // Prevent Chrome 67 and earlier from automatically showing the prompt
   e.preventDefault();
   // Stash the event so it can be triggered later.
